6b1238a63f79b8073e12fcb0cd8e1d476ed9c9f1
aglehmann <aleksander@flovik.no>
2019-08-19 16:17:21 +0200

    updated __init__.py to support multiple devices

--- ecovent/__init__.py
+++ ecovent/__init__.py
@@ -3,36 +3,41 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant.const import CONF_NAME, CONF_IP_ADDRESS, CONF_PORT
+from homeassistant.const import CONF_NAME, CONF_IP_ADDRESS, CONF_PORT, CONF_DEVICES
 from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.discovery import async_load_platform
 
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = 'ecovent'
+ECOVENT_DEVICES = 'ecovent_devices'
 
 CONFIG_SCHEMA = vol.Schema({
     DOMAIN: vol.Schema({
-        vol.Required(CONF_NAME): cv.string,
-        vol.Required(CONF_IP_ADDRESS): cv.string,
-        vol.Optional(CONF_PORT, default=4000): cv.port,
-    }),
-}, extra=vol.ALLOW_EXTRA)
-
+        vol.Required(CONF_DEVICES, default=[]): vol.All(cv.ensure_list, [dict]),
+    })
+},extra=vol.ALLOW_EXTRA)
 
 async def async_setup(hass, config):
     """Set up the Ecovent Fan component."""
     from ecovent import Fan
 
+    if ECOVENT_DEVICES not in hass.data:
+        hass.data[ECOVENT_DEVICES] = []
+
     conf = config[DOMAIN]
+    conf_devices = config[DOMAIN].get(CONF_DEVICES)
+    for device in conf_devices:
+        name = device.get(CONF_NAME)
+        ip_address = device.get(CONF_IP_ADDRESS)
 
-    name = conf.get(CONF_NAME)
-    ip_address = conf.get(CONF_IP_ADDRESS)
-    port = conf.get(CONF_PORT)
+        if device.get(CONF_PORT) is None:
+            port = 4000
+        else:
+            port = device.get(CONF_PORT)
 
-    fan = Fan(ip_address, name, port)
-
-    hass.data[DOMAIN] = fan
+        fan = Fan(ip_address, name, port)
+        hass.data[ECOVENT_DEVICES].append(fan)
 
     hass.async_create_task(
         async_load_platform(hass, 'fan', DOMAIN, {CONF_NAME: DOMAIN}, config)
